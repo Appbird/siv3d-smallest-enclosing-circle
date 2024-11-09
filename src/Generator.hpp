19c26dc176a1cd744e0fb5c78aec5880e736433f
appbird <project.appbird@gmail.com>
2024-11-09 20:32:58 +0900

    入力の自動生成関数を実装

--- src/Generator.hpp
+++ src/Generator.hpp
@@ -1,11 +1,31 @@
 #pragma once
 #include <Siv3D.hpp>
+#include <fstream>
 
 using Points = Array<Vec2>;
 
 NormalDistribution<double> StandardDistribution() {
     const Vec2 center_f = Scene::CenterF();
     return NormalDistribution<double>{center_f.x, center_f.x / 4};
+}
+
+Array<Vec2> GetInputFromFile(const FilePath& filepath) {
+    std::ifstream fin{filepath.toUTF8()};
+    assert(fin);
+    int32_t N; fin >> N;
+    Array<Vec2> result(N);
+    for (int32_t i = 0; i < N; i++) {
+        fin >> result[i].x >> result[i].y;
+    }
+    return result;
+}
+void WriteInputToFile(const FilePath& filepath, const Array<Vec2>& points) {
+    std::ofstream fout{filepath.toUTF8()};
+    assert(fout);
+    fout << points.size() << std::endl;
+    for (int32_t i = 0; i < points.size(); i++) {
+        fout << points[i].x << " " << points[i].y << std::endl;
+    }
 }
 
 /**
@@ -25,6 +45,35 @@ Points GeneratePoints(
     }
     return points;
 }
-Array<Vec2> GetFromStdin() {
-    
+
+Array<Vec2> GenerateHugeSmall(size_t N, DefaultRNG& rng) {
+    Array<Vec2> points(N);
+    NormalDistribution<double> dist{1e17, 500};
+    for (size_t i = 0; i < N; i++) {
+        points[i] = {dist(rng), dist(rng)};
+    }
+    return points;
+}
+Array<Vec2> GenerateHugeLarge(size_t N, DefaultRNG& rng) {
+    Array<Vec2> points(N);
+    NormalDistribution<double> dist{0, 1e17};
+    for (size_t i = 0; i < N; i++) {
+        points[i] = {dist(rng), dist(rng)};
+    }
+    return points;
+}
+
+Array<Vec2> GenerateShuffled(size_t N, DefaultRNG& rng) {
+    Array<Vec2> points(N);
+    NormalDistribution<double> dist{0, 1e17};
+    for (int32 i = 0; i < N; ++i)
+    {
+        points[i] = OffsetCircular{ Scene::CenterF(), (200.0 * i / N), (120_deg * i) };
+    }
+    return points;
+}
+
+void GenerateLargeInputs() {
+    DefaultRNG rng = GetDefaultRNG();
+    //= GeneratePoints(300, rng);
 }
