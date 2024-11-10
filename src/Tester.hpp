fe9cdef1fb6ee2c6d9a1e0529491e3d7282eabfa
appbird <project.appbird@gmail.com>
2024-11-10 23:39:30 +0900

    Implements Visualizer

--- src/Tester.hpp
+++ src/Tester.hpp
@@ -75,9 +75,37 @@ TestCaseResult TestSmallestEnclosing(const Array<Vec2>& points, const double eps
 
 void VisualizeTest(
     const Array<Vec2>& points,
-    const 
-) {
-
+    const TestCaseResult result,
+    const String& testcase_name,
+    const double EPSILON
+) {  
+    if (IsNaN(result.expected.r)) {
+        Print << U"expected circle has NaN value.";
+    }
+    if (IsNaN(result.actual.r)) {
+        Print << U"actual circle has NaN value.";
+    }
+    if (IsNaN(result.expected.r) or IsNaN(result.actual.r)) { return; }
+    {
+        // 点群とその最小包含円の描画
+        for (const Vec2& point : points)
+        {
+            const bool is_point_enclosed = contains(result.actual, point, EPSILON);
+            Circle{point, 2}.draw(
+                is_point_enclosed ? HSV{131, 0.78, 0.90} : HSV{131, 0, 1}
+            );
+        }
+        result.actual.drawFrame(3.0, HSV{131, 0.78, 0.90}).draw(HSV{131, 0.78, 0.90, 0.05});
+        if (not result.succeeded)
+        {
+            result.expected.drawFrame(3.0, HSV{35, 0.78, 0.90});
+        }
+    }
+    // 結果の詳細を右上に示す。
+    Print << testcase_name;
+    Print << U"N = {:3d}"_fmt(points.size());
+    Print << U"time: {:.9f}s"_fmt(result.process_time);
+    Print << U"Result: {}"_fmt(result.succeeded ? U"OK" : U"NG");
 }
 
 
@@ -128,28 +156,10 @@ void VisualRandomTest()
             }
         }
 
-        // 点群とその最小包含円の描画
-        for (const Vec2& point : points)
-        {
-            const bool is_point_enclosed = contains(result.actual, point, EPSILON);
-            Circle{point, 5}.draw(
-                is_point_enclosed ? HSV{131, 0.78, 0.90} : HSV{131, 0, 1}
-            );
-        }
-        result.actual.drawFrame(3.0, HSV{131, 0.78, 0.90}).draw(HSV{131, 0.78, 0.90, 0.05});
-        if (not result.succeeded or draw_expected_circle)
-        {
-            result.expected.drawFrame(3.0, HSV{35, 0.78, 0.90});
-        }
-
-        SimpleGUI::CheckBox(draw_expected_circle, U"Draw Expected", Vec2{ 600, 10 });
-        
-        // 結果の詳細を右上に示す。
-        Print << U"N = {:3d}"_fmt(points.size());
-        Print << U"time: {:.9f}s"_fmt(result.process_time);
+        VisualizeTest(points, result, U"random_tests", EPSILON);
         Print << U"average_time: {:.9f}s"_fmt(sum_cost_time / test_count);
         Print << U"test_count: {}"_fmt(test_count);
-        Print << U"Result: {}"_fmt(result.succeeded ? U"OK" : U"NG");
+        
 	}
 }
 
@@ -164,40 +174,89 @@ Array<Vec2> GetInputFromFile(const FilePath& filepath) {
     }
     return result;
 }
+Array<FilePath> ReadAllTestCases() {
+    const Array<FilePath> cases_handmade    = FileSystem::DirectoryContents(U"input-handmade");
+    const Array<FilePath> cases_auto        = FileSystem::DirectoryContents(U"input-auto");
+    Array<FilePath> all_cases;
+    std::ranges::copy(cases_handmade, std::back_inserter(all_cases));
+    std::ranges::copy(cases_auto, std::back_inserter(all_cases));
+    std::ranges::sort(all_cases);
+    return all_cases;
+}
 
 void FullTest(const double EPSILON = 1e-8) {
-    
+    // テスト処理
     Logger << U"[FullTest at {}]"_fmt(DateTime::Now());
-    int32 total_case = 0; 
-    int32 success = 0;
-    auto fulltest = [&](const FilePath& testset){
-        const Array<FilePath> paths = FileSystem::DirectoryContents(testset);
-        for (const FilePath& path : paths) {
-            total_case++;
+    Array<FilePath> all_cases = ReadAllTestCases();
+    Array<String> all_cases_judgestate(all_cases.size());
+    size_t current_testnumber = 0; 
+    size_t success = 0;
+    {
+        TextWriter logger{U"fulltest.log"};
+        for (const FilePath& path : all_cases) {
             const Array<Vec2> input = GetInputFromFile(path);
             const String casename = FileSystem::FileName(path);
             
             TestCaseResult result = TestSmallestEnclosing(input, EPSILON);
-            const String judge_state = result.succeeded ? U"\e[42m\e[37m[AC]\e[0m" : U"\e[43m\e[37m[WA]\e[0m";
+            const String judge_state = result.succeeded ? U"[AC]" : U"[WA]";
+            const String judge_state_cmd = result.succeeded ? U"\e[42m\e[37m[AC]\e[0m" : U"\e[43m\e[37m[WA]\e[0m";
             if (result.succeeded) { success++; }
             
-            const String TestCaseTitle = U"\n[{}] {}"_fmt(casename, judge_state);
-            Console << TestCaseTitle;
-            Console << TestCaseTitle;
-            Console << U"\ttime: {:.9f}s"_fmt(result.process_time);
-            Console << U"\texpected: (center, r) = ({}, {})"_fmt(result.expected.center, result.expected.r);
-            Console << U"\tactual:   (center, r) = ({}, {})"_fmt(result.actual.center, result.actual.r);
+            Console << U"\n[{}] {}"_fmt(casename, judge_state_cmd);
+            logger << U"\n[{}] {}"_fmt(casename, judge_state);
+            logger << U"\ttime: {:.9f}s"_fmt(result.process_time);
+            logger << U"\texpected: (center, r) = ({}, {})"_fmt(result.expected.center, result.expected.r);
+            logger << U"\tactual:   (center, r) = ({}, {})"_fmt(result.actual.center, result.actual.r);
+            all_cases_judgestate[current_testnumber] = judge_state;
+            current_testnumber++;
         }
-    };
-    fulltest(U"input-handmade");
-    fulltest(U"input-auto");
-    Console << U"[AC] x {} / {}"_fmt(success, total_case);
-    Console << U"result: {}"_fmt((success == total_case) ? U"[AC]" : U"[WA]");
+        logger << U"[AC] x {} / {}"_fmt(success, all_cases.size());
+        logger << U"result: {}"_fmt((success == all_cases.size()) ? U"[AC]" : U"[WA]");
+    }
+    
+    // 現在の問題例を表す点群
+    Array<FilePath> all_cases_title(all_cases.size());
+    for (size_t i = 0; i < all_cases.size(); i++) {
+        all_cases_title[i] = U"[{}] {}"_fmt(all_cases_judgestate[i], FileSystem::FileName(all_cases[i]));
+    }
 
-
+    // テスト結果のビジュアライザ
+    Array<Vec2> current_instance;
+    TestCaseResult testcase_result;
+    ListBoxState case_list { all_cases_title };
+    Optional<size_t> current_selection = 0;
+    bool if_drawExpectedCircle = false;
+    const RectF view_area = {0, 0, 600, 600};
+    Mat3x2 viewport_affine;
     while (System::Update()) {
-        if (KeyLeft) {
-
+        
+        ClearPrint();
+        if (case_list.selectedItemIndex and current_selection != case_list.selectedItemIndex) {
+            current_selection = case_list.selectedItemIndex;
+            current_instance = GetInputFromFile(all_cases[*current_selection]);
+            testcase_result = TestSmallestEnclosing(current_instance);
+            
+            // 描画すべき範囲を求める。
+            Vec2 min_point = {1e18, 1e18}, max_point = {-1e18, -1e18};
+            for (const Vec2& point : current_instance) {
+                min_point.x = Min(min_point.x, point.x);
+                min_point.y = Min(min_point.y, point.y);
+                max_point.x = Max(max_point.x, point.x);
+                max_point.y = Max(max_point.y, point.y);
+            }
+            RectF region_points = RectF::FromPoints(min_point, max_point).stretched(5);
+            SizeF size_region_display = SizeF{1., 1.} * region_points.size.maxComponent();
+            RectF region_display = RectF{Arg::center = region_points.center(), size_region_display};
+            Console << region_points;
+            viewport_affine =
+                Mat3x2::Scale(view_area.w / region_display.w)*
+                Mat3x2::Translate(view_area.center())*
+                Mat3x2::Translate(-region_display.center());
         }
+        if (case_list.selectedItemIndex) {
+            Transformer2D transformer{viewport_affine};
+            VisualizeTest(current_instance, testcase_result, case_list.items[*current_selection], EPSILON);
+        }
+        SimpleGUI::ListBox(case_list, Vec2{ 500, 30 }, 300, 156);
     }
 }
