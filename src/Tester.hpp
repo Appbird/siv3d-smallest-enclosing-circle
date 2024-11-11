8213652ae13d96fd3ffa4f08b98b23abd6ab7599
appbird <project.appbird@gmail.com>
2024-11-11 16:02:16 +0900

    ConvexHullが空になる場合、および点数が少ない場合についてNaiveのバグを修正

--- src/Tester.hpp
+++ src/Tester.hpp
@@ -17,31 +17,36 @@ struct TestCaseResult {
 
 // テスト用コード by ラクラムシさん
 // 愚直な方法で最小包含円問題を解く
-Circle SmallestEnclosingCircleNaive(const Array<Vec2>& points, const double epsilon = 1e-12);
+Circle SmallestEnclosingCircleNaive(const Array<Vec2>& points, const double epsilon = 1e-8);
 
 /**
  * @brief ある点群に対して、`SmallestEnclosingCircle`の計算した円が本当に最小かつ内包かを確認する。
  */
-TestCaseResult TestSmallestEnclosing(const Array<Vec2>& points, const double epsilon = 1e-12);
+TestCaseResult TestSmallestEnclosing(const Array<Vec2>& points, const double epsilon = 1e-8);
 
 
 // テスト用コード by ラクラムシさん
 // 愚直な方法（四重ループ）で最小包含円問題を解く
 Circle SmallestEnclosingCircleNaive(const Array<Vec2>& points, const double epsilon)
 {
+    // #FIXED: convexHullが空だった場合は元のpointsに戻すように修正
     const auto convexHull = Geometry2D::ConvexHull(points).outer();
-    const auto n = convexHull.size();
-    if (n == 0) {
-        if (points.size() == 0) { return Circle{}; }
-        return Circle{points[0], 0};
-    }
+    const auto eliminated_points = (convexHull.empty()) ? points : convexHull;
+    const auto n = eliminated_points.size();
+
+    // #FIXED  by Nachia
+    // 点のサイズが2以下だった時に挙動がおかしくなる問題を修正
+    if (eliminated_points.size() == 0) { return Circle{}; }
+    if (eliminated_points.size() == 1) { return Circle{eliminated_points[0], 0}; }
+    if (eliminated_points.size() == 2) { return Circle{eliminated_points[0], eliminated_points[1]}; }
+    
     Circle smallest{ 0, 0, Math::Inf };
     for (size_t i = 2; i < n; ++i)
     for (size_t j = 1; j < i; ++j)
     for (size_t k = 0; k < j; ++k)
     {
-        const auto c = SmallestEnclosingCircle(convexHull[i], convexHull[j], convexHull[k]);
-        if ((c.r < smallest.r) && convexHull.all([&](const Vec2& p) { return p.distanceFromSq(c.center) <= Math::Square(c.r + epsilon); }))
+        const auto c = SmallestEnclosingCircle(eliminated_points[i], eliminated_points[j], eliminated_points[k]);
+        if ((c.r < smallest.r) && eliminated_points.all([&](const Vec2& p) { return p.distanceFromSq(c.center) <= Math::Square(c.r + epsilon); }))
         {
             smallest = c;
         }
@@ -73,33 +78,25 @@ TestCaseResult TestSmallestEnclosing(const Array<Vec2>& points, const double eps
     };
 };
 
-void VisualizeTest(
+void DrawTestResult(
     const Array<Vec2>& points,
     const TestCaseResult result,
     const String& testcase_name,
-    const double EPSILON
-) {  
-    if (IsNaN(result.expected.r)) {
-        Print << U"expected circle has NaN value.";
-    }
-    if (IsNaN(result.actual.r)) {
-        Print << U"actual circle has NaN value.";
-    }
-    if (IsNaN(result.expected.r) or IsNaN(result.actual.r)) { return; }
+    const double EPSILON,
+    const double point_scaling = 1
+) { 
+    // 点群とその最小包含円の描画
+    for (const Vec2& point : points)
     {
-        // 点群とその最小包含円の描画
-        for (const Vec2& point : points)
-        {
-            const bool is_point_enclosed = contains(result.actual, point, EPSILON);
-            Circle{point, 2}.draw(
-                is_point_enclosed ? HSV{131, 0.78, 0.90} : HSV{131, 0, 1}
-            );
-        }
-        result.actual.drawFrame(3.0, HSV{131, 0.78, 0.90}).draw(HSV{131, 0.78, 0.90, 0.05});
-        if (not result.succeeded)
-        {
-            result.expected.drawFrame(3.0, HSV{35, 0.78, 0.90});
-        }
+        const bool is_point_enclosed = contains(result.actual, point, EPSILON);
+        Circle{point, 2 * point_scaling}.draw(
+            is_point_enclosed ? HSV{131, 0.78, 0.90} : HSV{131, 0, 1}
+        );
+    }
+    result.actual.drawFrame(2.0 * point_scaling, HSV{131, 0.78, 0.90}).draw(HSV{131, 0.78, 0.90, 0.05});
+    if (not result.succeeded)
+    {
+        result.expected.drawFrame(2.0 * point_scaling, HSV{35, 0.78, 0.90});
     }
     // 結果の詳細を右上に示す。
     Print << testcase_name;
@@ -120,12 +117,10 @@ void VisualRandomTest()
     const double refresh_interval = 1;
     /** 新たな点群が生成されてから何秒経ったか */
     double t = refresh_interval;
-    bool draw_expected_circle = false;
 
     /** 乱数生成器。点群の生成に使用する。 */
     auto& rng = GetDefaultRNG();
     Array<Vec2> points;
-    const Vec2 center_f = Scene::CenterF();
     
     const size_t N = 10000;
     constexpr double EPSILON = 1e-8;
@@ -156,7 +151,7 @@ void VisualRandomTest()
             }
         }
 
-        VisualizeTest(points, result, U"random_tests", EPSILON);
+        DrawTestResult(points, result, U"random_tests", EPSILON);
         Print << U"average_time: {:.9f}s"_fmt(sum_cost_time / test_count);
         Print << U"test_count: {}"_fmt(test_count);
         
@@ -184,13 +179,15 @@ Array<FilePath> ReadAllTestCases() {
     return all_cases;
 }
 
+
+
 void FullTest(const double EPSILON = 1e-8) {
     // テスト処理
     Logger << U"[FullTest at {}]"_fmt(DateTime::Now());
     Array<FilePath> all_cases = ReadAllTestCases();
-    Array<String> all_cases_judgestate(all_cases.size());
-    size_t current_testnumber = 0; 
+    Array<TestCaseResult> test_results;
     size_t success = 0;
+    // すでに用意されているテストケースをすべて確認していく。
     {
         TextWriter logger{U"fulltest.log"};
         for (const FilePath& path : all_cases) {
@@ -198,6 +195,7 @@ void FullTest(const double EPSILON = 1e-8) {
             const String casename = FileSystem::FileName(path);
             
             TestCaseResult result = TestSmallestEnclosing(input, EPSILON);
+            test_results.push_back(result);
             const String judge_state = result.succeeded ? U"[AC]" : U"[WA]";
             const String judge_state_cmd = result.succeeded ? U"\e[42m\e[37m[AC]\e[0m" : U"\e[43m\e[37m[WA]\e[0m";
             if (result.succeeded) { success++; }
@@ -207,8 +205,6 @@ void FullTest(const double EPSILON = 1e-8) {
             logger << U"\ttime: {:.9f}s"_fmt(result.process_time);
             logger << U"\texpected: (center, r) = ({}, {})"_fmt(result.expected.center, result.expected.r);
             logger << U"\tactual:   (center, r) = ({}, {})"_fmt(result.actual.center, result.actual.r);
-            all_cases_judgestate[current_testnumber] = judge_state;
-            current_testnumber++;
         }
         logger << U"[AC] x {} / {}"_fmt(success, all_cases.size());
         logger << U"result: {}"_fmt((success == all_cases.size()) ? U"[AC]" : U"[WA]");
@@ -218,7 +214,7 @@ void FullTest(const double EPSILON = 1e-8) {
     // タイトルを含める
     Array<FilePath> all_cases_title(all_cases.size());
     for (size_t i = 0; i < all_cases.size(); i++) {
-        all_cases_title[i] = U"[{}] {}"_fmt(all_cases_judgestate[i], FileSystem::FileName(all_cases[i]));
+        all_cases_title[i] = U"[{}] {}"_fmt(test_results[i].succeeded ? U"AC" : U"WA", FileSystem::FileName(all_cases[i]));
     }
     // 問題例をあらわす点群を格納する配列
     Array<Vec2> current_instance;
@@ -228,18 +224,19 @@ void FullTest(const double EPSILON = 1e-8) {
     ListBoxState case_list { all_cases_title };
     // 現在選んでいる問題例の番号
     Optional<size_t> current_selection = 0;
-    bool if_drawExpectedCircle = false;
     const RectF view_area = {0, 0, 600, 600};
     Mat3x2 viewport_affine;
+    double point_scale = 1;
     while (System::Update()) {
         
         ClearPrint();
         if (case_list.selectedItemIndex and current_selection != case_list.selectedItemIndex) {
+            // 問題例の計算
             current_selection = case_list.selectedItemIndex;
             current_instance = GetInputFromFile(all_cases[*current_selection]);
-            testcase_result = TestSmallestEnclosing(current_instance);
+            testcase_result = test_results[*current_selection];
             
-            // 描画すべき範囲を求める。
+            // 描画すべき範囲を求めておき、後でTransformer2Dに適用する。
             Vec2 min_point = {1e18, 1e18}, max_point = {-1e18, -1e18};
             for (const Vec2& point : current_instance) {
                 min_point.x = Min(min_point.x, point.x);
@@ -250,15 +247,15 @@ void FullTest(const double EPSILON = 1e-8) {
             RectF region_points = RectF::FromPoints(min_point, max_point).stretched(5);
             SizeF size_region_display = SizeF{1., 1.} * region_points.size.maxComponent();
             RectF region_display = RectF{Arg::center = region_points.center(), size_region_display};
-            Console << region_points;
+            point_scale = view_area.w / region_display.w;
             viewport_affine =
-                Mat3x2::Scale(view_area.w / region_display.w)*
-                Mat3x2::Translate(view_area.center())*
-                Mat3x2::Translate(-region_display.center());
+                Mat3x2::Translate(-region_display.center()) *
+                Mat3x2::Scale(point_scale) *
+                Mat3x2::Translate(view_area.center());
         }
         if (case_list.selectedItemIndex) {
             Transformer2D transformer{viewport_affine};
-            VisualizeTest(current_instance, testcase_result, case_list.items[*current_selection], EPSILON);
+            DrawTestResult(current_instance, testcase_result, case_list.items[*current_selection], EPSILON, 1/point_scale);
         }
         SimpleGUI::ListBox(case_list, Vec2{ 500, 30 }, 300, 156);
     }
