42f36f657ea5f0c767faf15b900cff6cafd15798
appbird <project.appbird@gmail.com>
2024-11-12 20:48:15 +0900

    GenerateAndTestモードを追加する。

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,16 +4,21 @@
 #include "Generator.hpp"
 
 enum class ApplicationMode {
+    GenerateAndTest,
     FullTest,
     RandomTest,
     GenerateInput
 };
-constexpr ApplicationMode mode = ApplicationMode::FullTest;
+constexpr ApplicationMode mode = ApplicationMode::GenerateAndTest;
 // Modify this line to change the behavior of this application.
 constexpr double EPSILON = 1e-8;
 
 void Main() {
     switch (mode) {
+        case ApplicationMode::GenerateAndTest:
+            GenerateLargeInputs();
+            FullTest(EPSILON);
+            break;
         case ApplicationMode::FullTest:
             FullTest(EPSILON);
             break;
